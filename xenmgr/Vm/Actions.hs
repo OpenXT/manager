707ed5da685cc406cd2ca9beada96d3df230f38a
crogers1 <crogers1@binghamton.edu>
2014-11-14 16:12:30 -0500

    Update Actions.hs
    Fix whitespace issue

--- xenmgr/Vm/Actions.hs
+++ xenmgr/Vm/Actions.hs
@@ -442,6 +442,7 @@ removeVm uuid =
        -- Away from database
        dbRm $ "/vm/" ++ show uuid
        dbRm $ "/dom-store/" ++ show uuid
+       
        -- Need to quit xenvm
        -- FIXME: cleanly stop monitoring events
        removeDefaultEvents uuid	--cleanly...stop monitoring events
