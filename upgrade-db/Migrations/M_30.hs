9cfb086298ce56466cd4baf5807e95ca7ef22bea
Jed <lejosnej@ainfosec.com>
2016-08-22 10:25:26 -0400

    Move the USB mouse rule to a new migration script
    OXT-582
    
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- upgrade-db/Migrations/M_30.hs
+++ upgrade-db/Migrations/M_30.hs
@@ -1,6 +1,5 @@
 --
--- Copyright (c) 2015 Assured Information Security, Inc. <lejosnej@ainfosec.com>
--- Copyright (c) 2014 Citrix Systems, Inc.
+-- Copyright (c) 2016 Assured Information Security, Inc. <lejosnej@ainfosec.com>
 -- 
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -19,16 +18,16 @@
 
 {-# LANGUAGE PatternGuards #-}
 
--- description: add default USB policy
--- date: 07/30/2015
+-- description: add policy bit to deny USB mouse assignment to guests
+-- date: 08/22/2016
 
-module Migrations.M_26 (migration) where
+module Migrations.M_30 (migration) where
 
 import UpgradeEngine
 
 migration = Migration {
-              sourceVersion = 26
-            , targetVersion = 27
+              sourceVersion = 30
+            , targetVersion = 31
             , actions = act
             }
 
@@ -36,8 +35,6 @@ migration = Migration {
 act :: IO ()
 act = xformPrimaryJSON $ policy
   where
-    policy = jsSet "/usb-rules/9999/command"         (jsBoxString "allow") .
-             jsSet "/usb-rules/9999/description"     (jsBoxString "Allow everything else") .
-             jsSet "/usb-rules/9900/device/keyboard" (jsBoxString "1") .
-             jsSet "/usb-rules/9900/command"         (jsBoxString "deny") .
-             jsSet "/usb-rules/9900/description"     (jsBoxString "Deny keyboard passthrough to all VMs")
+    policy = jsSet "/usb-rules/9901/device/mouse"    (jsBoxString "1") .
+             jsSet "/usb-rules/9901/command"         (jsBoxString "deny") .
+             jsSet "/usb-rules/9901/description"     (jsBoxString "Deny mouse passthrough to all VMs")
