f38a42c31ce9041b256f51afa8b3ae2c11ff67cb
Mahantesh Salimath <mahantesh.openxt@gmail.com>
2019-03-05 17:37:44 -0500

    fix disk virtual path
    OXT-1123
    
    Signed-off-by: Mahantesh Salimath <mahantesh.openxt@gmail.com>

--- xenmgr/Vm/Queries.hs
+++ xenmgr/Vm/Queries.hs
@@ -320,7 +320,7 @@ pickDiskVirtPath uuid = nextFreeVirtPath . M.elems <$> getDisks uuid
 nextFreeVirtPath :: [Disk] -> String
 nextFreeVirtPath disks = head . filter (not_taken disks) $ candidates
   where
-    candidates = [ "hda", "hdb", "hdc", "hdd" ] ++ map (\s -> "xvd" ++ [s]) ['a'..'z']
+    candidates = map (\s -> "hd" ++ [s]) ['a'..'z']
     not_taken disks virtp = not $ virtp `elem` (map diskDevice disks)
 
 envIsoDir :: FilePath
